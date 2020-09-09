3b1c4a19acf2889d0840e427e07248c6460f0ee1
Pradeep Balasubramanian <b4upradeep@gmail.com>
2020-09-09 13:11:28 +0530

    Initial Commit

---
+++ README.markdown
@@ -0,0 +1,3 @@
+# Personal Portfolio - Pradeep Balasubramanian
+
+A Personal Portfolio Page of Pradeep Balasubramanian
---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+
+   $(function() {
+   //    $("nav div").mouseenter(function() {
+   //       $(this).css(
+   //         {"background-color":'#eae1c0'});
+   //       $(this).children().css({'color':'#151E3F'});
+   //
+   //    });
+   //
+   //   $("nav div").mouseleave(function() {
+   //       $(this).css(
+   //         {"background-color":'#151E3F'});
+   //               $(this).children().css({'color':'#eae1c0'})
+   //
+   //    });
+   //
+   $( "#collapsible-accordian" ).accordion({
+      collapsible: true, heightStyle : "content",active:false
+    });
+   });
